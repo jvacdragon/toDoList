cd99f8704393bdff3fa1adff7249c9d7bfadf432
jvacdragon <joao_aguiarcosta@hotmail.com>
2023-01-03 21:22:01 -0300

    small changes

--- src/components/Head/Head.js
+++ src/components/Head/Head.js
@@ -2,11 +2,11 @@ const Head = () => {
   return (
     <header className="text-center my-16 ">
       <div className="my-8">
-        <h1 className="">Your Own To Do List</h1>
-        <h2 className="">Don't think, just DO IT!</h2>
+        <h1 className="">Your Daily To Do List</h1>
+        <h2 className="">Organize yourself!</h2>
       </div>
 
-      <div className="headRotate">Start to organize your duties</div>
+      <div className="headRotate">Start to organize your own life</div>
     </header>
   );
 };
